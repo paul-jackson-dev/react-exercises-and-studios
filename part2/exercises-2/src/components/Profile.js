87dc4b260c6f12958754d46fe0d0404756ebd3a5
Mango <greatdogsite@gmail.com>
2023-09-21 12:05:46 -0500

    completed Part 2 Exercises

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -2,17 +2,29 @@ import './styles.css';
 import Button from './Button';
 import oceans from'./oceans.json';
 
-
+let listItem = oceans.map( (ocean) => (
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+     <img src={ocean.image} alt={ocean.name} className="img"></img>
+     <h1>{ocean.name}</h1>
+     <h2>Fun Facts:</h2>
+      <ul>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+      </ul>
+      <Button />
+   </div>
+   )
+);
 
 function Profile()
 {
-   let ListItems = oceans.map( (ocean) => (
-      <h1 key={ocean.id}>{ocean.id}</h1>
-   ));
    return(
       <> 
-         <ListItems />
-         <Button />
+         <ul>
+            {listItem}
+            {/* <Button /> */}
+         </ul>
       </>  
    );
 }
